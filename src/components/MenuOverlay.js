7347fdaeb9a15c210556494f71978a4d637a3133
Justin W Hall <justinwhall@me.com>
2019-12-12 15:16:33 -0700

    update menu

--- src/components/MenuOverlay.js
+++ src/components/MenuOverlay.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from 'react'
+import { Link } from 'gatsby'
 
 class MenuOverlay extends React.Component {
   state = {
-    open: false
+    open: false,
   }
 
   toggleMenu = () => {
     this.setState({
-      open: !this.state.open
+      open: !this.state.open,
     })
   }
 
@@ -16,33 +16,41 @@ class MenuOverlay extends React.Component {
     return (
       <React.Fragment>
         <div
-        onClick={this.toggleMenu}
-        id="nav-icon"
-        className={`menu-toggle ${this.state.open ? 'fixed' : null}`}
-        aria-controls="primary-menu"
-        aria-expanded="false"
+          onClick={this.toggleMenu}
+          id="nav-icon"
+          className={`menu-toggle ${this.state.open ? 'fixed' : null}`}
+          aria-controls="primary-menu"
+          aria-expanded="false"
         >
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
+          <span />
+          <span />
+          <span />
+          <span />
           <div className="screen-reader-text">Menu</div>
         </div>
         <nav id="nav" className={this.state.open ? 'in' : null}>
           <div className="menu-main-nav-menu-container">
             <ul id="primary-menu" className="menu">
-              <li><Link to="/">HOME</Link></li>
-              <li><Link to="/work/">WORK</Link></li>
-              <li><Link to="/web-development-blog/">BLOG</Link></li>
-              <li><a target="_blank" href="http://windsorup.com/">MAINTENANCE</a></li>
-              <li><a targset="_blank" href="https://www.littlebot.io">PLUGINS</a></li>
-              <li><Link to="/contact/">CONTACT</Link></li>
+              <li>
+                <Link to="/">HOME</Link>
+              </li>
+              <li>
+                <Link to="/web-development-blog/">BLOG</Link>
+              </li>
+              <li>
+                <a targset="_blank" href="https://www.littlebot.io">
+                  PLUGINS
+                </a>
+              </li>
+              <li>
+                <Link to="/contact/">CONTACT</Link>
+              </li>
             </ul>
           </div>
         </nav>
       </React.Fragment>
-    );
+    )
   }
 }
 
-export default MenuOverlay;
+export default MenuOverlay
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import MenuOverlay from '../components/MenuOverlay';
-import Social from '../components/Social';
-import Faqs from '../components/Faqs';
-import ContactForm from '../components/ContactForm';
-import Footer from '../components/Footer';
-import Mountain from '../images/mtn.svg';
-import Heart from '../images/heart.svg';
-import '../sass/milligram.sass';
+import React from 'react'
+import Helmet from 'react-helmet'
+import MenuOverlay from '../components/MenuOverlay'
+import Social from '../components/Social'
+import Faqs from '../components/Faqs'
+import ContactForm from '../components/ContactForm'
+import Footer from '../components/Footer'
+import Mountain from '../images/mtn.svg'
+import Heart from '../images/heart.svg'
+import '../sass/milligram.sass'
 
 const title = [
-  'Web Developer',
-  'Front-End Engineer',
+  'Front End Engineer',
   'UIUX Advocate',
   'Software Builder',
   'Open Source Contributor',
@@ -20,46 +19,46 @@ const title = [
 ]
 export default class Home extends React.Component {
   state = {
-    mtnOpacity : 0,
+    mtnOpacity: 0,
     hearClass: '',
     index: 0,
     title: title[0],
   }
 
   tick() {
-    const { index } = this.state;
-    const nextIndex = title.length === (index + 1) ? 0 : index + 1;
+    const { index } = this.state
+    const nextIndex = title.length === index + 1 ? 0 : index + 1
 
     this.setState(prevState => ({
       index: nextIndex,
       title: title[nextIndex],
-    }));
+    }))
   }
 
   componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 2000);
+    this.interval = setInterval(() => this.tick(), 2000)
 
     this.mtn = setTimeout(() => {
       this.setState({
-        mtnOpacity: 1
+        mtnOpacity: 1,
       })
-    }, 100);
+    }, 100)
 
     this.heart = setTimeout(() => {
       this.setState({
-        heartClass: "start"
+        heartClass: 'start',
       })
-    }, 700);
+    }, 700)
   }
 
   componentWillUnmount() {
-      clearInterval(this.interval);
-      clearTimeout(this.mtn);
-      clearTimeout(this.heart);
+    clearInterval(this.interval)
+    clearTimeout(this.mtn)
+    clearTimeout(this.heart)
   }
 
   scrollToMore = () => {
-    this.more.scrollIntoView( { block: 'start', behavior: 'smooth' } );
+    this.more.scrollIntoView({ block: 'start', behavior: 'smooth' })
   }
 
   render() {
@@ -74,9 +73,12 @@ export default class Home extends React.Component {
               <div className="my-name">My name is Justin Windsor Hall</div>
             </div>
 
-            <div className="secondary">I'm a Denver, CO based <span className='l-blue'>{this.state.title}</span></div>
+            <div className="secondary">
+              I'm a Denver, CO based{' '}
+              <span className="l-blue">{this.state.title}</span>
+            </div>
             <div className="mountain">
-              <Mountain style={{'opacity': this.state.mtnOpacity}} />
+              <Mountain style={{ opacity: this.state.mtnOpacity }} />
             </div>
             <div className={`heart ${this.state.heartClass}`}>
               <div className="secondary code">Code</div>
@@ -88,34 +90,98 @@ export default class Home extends React.Component {
               </svg>
             </div>
           </div>
-          <div ref={more => this.more = more} className="more-about p-top-bottom">
+          <div
+            ref={more => (this.more = more)}
+            className="more-about p-top-bottom"
+          >
             <div className="container">
               <h2>Modern Websites &amp; Software</h2>
               <p>
                 {`I'm a Senior Developer at `}
-                <a target="_blank" rel="noopener noreferrer" href="https://sendgrid.com">SendGrid,</a>
-                {' '}
-                <a target="_blank" rel="noopener noreferrer" href="https://www.justinwhall.com/">Freelancer,</a>
-                {' '}
-                <a target="_blank" rel="noopener noreferrer" href="https://www.littlebot.io">Plugin Developer,</a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://sendgrid.com"
+                >
+                  SendGrid,
+                </a>{' '}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.justinwhall.com/"
+                >
+                  Freelancer,
+                </a>{' '}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.littlebot.io"
+                >
+                  Plugin Developer,
+                </a>
                 {' and half of '}
-                <a target="_blank" rel="noopener noreferrer" href="https://staticfuse.com">Static Fuse</a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://staticfuse.com"
+                >
+                  Static Fuse
+                </a>
                 {`. I'm especially interested in the headless CMS space, serverless, Gatsby and WordPress. `}
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/justinwhall/justinwhall-frontend">This website </a> {`was built with WordPress + Gatsby + AWS. ❤️`}
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://github.com/justinwhall/justinwhall-frontend"
+                >
+                  This website{' '}
+                </a>{' '}
+                {`was built with WordPress + Gatsby + AWS. ❤️`}
               </p>
               <p>
                 {`Solving real-world problems with software is my favorite part of my job. It's not only challenging but exceedingly rewarding. I love what I do.`}
               </p>
               <p>
                 {'Offline, I live just outside of Denver, CO in '}
-                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/p/BWjcatODFID/?taken-by=justinwindhall">Golden</a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.instagram.com/p/BWjcatODFID/?taken-by=justinwindhall"
+                >
+                  Golden
+                </a>
                 {`. I'm an `}
-                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/p/BCEyvSjDgj-/?taken-by=justinwindhall">avid mountain biker, </a>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/p/BPjVFP0lSq7/?taken-by=justinwindhall">skier</a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.instagram.com/p/BCEyvSjDgj-/?taken-by=justinwindhall"
+                >
+                  avid mountain biker,{' '}
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.instagram.com/p/BPjVFP0lSq7/?taken-by=justinwindhall"
+                >
+                  skier
+                </a>
                 {' and '}
-                <a target="_blank" rel="noopener noreferrer" href="https://www.porchdrinking.com/wp-content/uploads/2016/09/how-to-enjoy-pumpkin-beer.jpg"> opinionated beer drinker</a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.porchdrinking.com/wp-content/uploads/2016/09/how-to-enjoy-pumpkin-beer.jpg"
+                >
+                  {' '}
+                  opinionated beer drinker
+                </a>
                 {'. I also share a birthday with '}
-                <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/p/Ba9ypNrDD0J/?taken-by=justinwindhall">my sister</a>.
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.instagram.com/p/Ba9ypNrDD0J/?taken-by=justinwindhall"
+                >
+                  my sister
+                </a>
+                .
               </p>
             </div>
           </div>
@@ -128,4 +194,3 @@ export default class Home extends React.Component {
     )
   }
 }
-
