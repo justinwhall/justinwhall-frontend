b45218da07939087ece9a9dd6cada4cc25cd66f0
Justin W Hall <justinwhall@me.com>
2020-01-04 12:11:02 -0700

    2020

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'gatsby';
+import React from 'react'
+import { Link } from 'gatsby'
 
 const Footer = () => (
   <footer id="colophon" className="site-footer">
@@ -8,22 +8,43 @@ const Footer = () => (
         <div className="footer-menu column">
           <div className="menu-main-nav-menu-container">
             <ul id="primary-menu" className="menu">
-              <li className="menu-item"><Link to="/">HOME</Link></li>
-              <li className="menu-item"><Link to="/work/">WORK</Link></li>
-              <li className="menu-item"><Link to="/web-development-blog/">BLOG</Link></li>
-              <li className="menu-item"><a target="_blank" rel="noopener noreferrer" href="http://windsorup.com/">MAINTENANCE</a></li>
-              <li className="menu-item"><a target="_blank" rel="noopener noreferrer" href="https://www.littlebot.io">PLUGINS</a></li>
-              <li className="menu-item"><Link to="/contact/">CONTACT</Link></li>
+              <li className="menu-item">
+                <Link to="/">HOME</Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/work/">WORK</Link>
+              </li>
+              <li className="menu-item">
+                <Link to="/web-development-blog/">BLOG</Link>
+              </li>
+              <li className="menu-item">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="http://windsorup.com/"
+                >
+                  MAINTENANCE
+                </a>
+              </li>
+              <li className="menu-item">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.littlebot.io"
+                >
+                  PLUGINS
+                </a>
+              </li>
+              <li className="menu-item">
+                <Link to="/contact/">CONTACT</Link>
+              </li>
             </ul>
-          </div>
-          {' '}
+          </div>{' '}
         </div>
-        <div className="column copy">
-          © 2018 Justin W Hall
-        </div>
+        <div className="column copy">© 2020 Justin W Hall</div>
       </div>
     </div>
   </footer>
-);
+)
 
-export default Footer;
+export default Footer
