41cce76d552b7702f9c459720852b0d4be795a99
justinwhall <justinwhall@me.com>
2020-01-09 13:48:40 -0700

    update title and FAQ

--- src/components/Faqs.js
+++ src/components/Faqs.js
@@ -1,35 +1,31 @@
-import React from 'react';
+import React from 'react'
 
 const questions = [
   {
     question: 'How much do you cost?',
-    answer: 'This depends on a number of factors. Specifically but not limited to, project size, my role in the project and whether there is ongoing work to be had. As a general rule, I charge 100/hour and don\'t take projects under 3k.'
+    answer:
+      "This depends on a number of factors. Specifically but not limited to, project size, my role in the project and whether there is ongoing work to be had. As a general rule, I charge 100/hour and don't take projects under 3k.",
   },
   {
     question: 'Can your provide hosting?',
-    answer: 'Yes, absolutely. I have a lot of experience with WordPress hosting and would be happy to provide a number of different options.'
-  },
-  {
-    question: 'Do you only work with WordPress?',
-    answer: 'No. While I do a lot, I also have experience with Node and very much enjoy working with JavaScript especially GatsbyJS and NextJS.',
-  },
-  {
-    question: 'Design? SEO? Paid Advertising?',
-    answer: 'No. I am a developer and it is my strong belief that one can not do many professions well. That being said, I\'ve worked very closely with designers and feel as though my attention to design and UX is a strong skill of mine.',
+    answer:
+      'Yes, absolutely. I have a lot of experience with WordPress hosting and would be happy to provide a number of different options.',
   },
   {
     question: 'Can you give a fixed quote?',
-    answer: 'Providing you provide proper project specs and designs (if applicable), I can provide an estimate for your project broken out by line item.',
+    answer:
+      'Providing you provide proper project specs and designs (if applicable), I can provide an estimate for your project broken out by line item.',
   },
   {
     question: 'Where are you from?',
-    answer: 'I grew up just North of Boston. I left for boarding school in \'96 and never moved back. I miss the seafood, Fall and Fenway Park. I don\'t miss the weather.',
+    answer:
+      "I grew up just North of Boston. I left for boarding school in '96 and never moved back. I miss the seafood, Fall and Fenway Park. I don't miss the weather.",
   },
 ]
 
 class FAQ extends React.Component {
   state = {
-    open: false
+    open: false,
   }
 
   toggleFAQ = () => {
@@ -39,20 +35,15 @@ class FAQ extends React.Component {
   }
 
   render() {
-
-    const {
-      question,
-      answer,
-    } = this.props;
+    const { question, answer } = this.props
 
     return (
-      <div onClick={this.toggleFAQ} className={`single-faq ${this.state.open && 'in'}`}>
-        <h4 className="question">
-          {question}
-        </h4>
-        <div className="answer">
-          {answer}
-        </div>
+      <div
+        onClick={this.toggleFAQ}
+        className={`single-faq ${this.state.open && 'in'}`}
+      >
+        <h4 className="question">{question}</h4>
+        <div className="answer">{answer}</div>
       </div>
     )
   }
@@ -63,11 +54,12 @@ const Faqs = () => (
     <div className="container">
       <h2>Questions I Answer Frequently</h2>
       <div className="questions">
-          {questions.map(question => <FAQ key={question.answer} {...question}/>
-          )}
+        {questions.map(question => (
+          <FAQ key={question.answer} {...question} />
+        ))}
       </div>
     </div>
   </div>
-);
+)
 
-export default Faqs;
+export default Faqs
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import MenuOverlay from "./MenuOverlay";
-import Navbar from './Navbar';
-import PageHeader from './PageHeader';
-import Footer from './Footer';
-import '../sass/milligram.sass';
+import React from 'react'
+import Helmet from 'react-helmet'
+import MenuOverlay from './MenuOverlay'
+import Navbar from './Navbar'
+import PageHeader from './PageHeader'
+import Footer from './Footer'
+import '../sass/milligram.sass'
 
 const TemplateWrapper = ({ children, title, breadCrumbs, date }) => (
   <div>
     <MenuOverlay />
-    <Helmet title={`${title} | Denver & Boulder Colorado WordPress Developer & Designer`} />
+    <Helmet title={`${title} | Denver & Boulder Colorado Front End Engineer`} />
     <Navbar />
     <PageHeader title={title} breadCrumbs={breadCrumbs} date={date} />
     <div>{children}</div>
